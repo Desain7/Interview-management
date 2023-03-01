86f04cbc2744d33e311411881e5a6406a5c4e837
Desain7 <1810479856@qq.com>
2023-03-01 15:11:34 +0800

    init: 初始化项目配置

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,29 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+import { useIntl } from 'umi';
+
+const Footer: React.FC = () => {
+  const intl = useIntl();
+  const defaultMessage = intl.formatMessage({
+    id: 'app.copyright.produced',
+    defaultMessage: 'Desain7',
+  });
+
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <DefaultFooter
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        {
+          key: 'github',
+          title: <GithubOutlined />,
+          href: 'https://github.com/Desain7',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
+
+export default Footer;
